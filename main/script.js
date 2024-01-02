613ab6c4a39928553a73b44bedc5eb5c70d48197
Ethan Breitkreutz <eteensmash@gmail.com>
2024-01-02 13:21:14 -0800

    all buttons declareed and pulled from the DOM

--- main/script.js
+++ main/script.js
@@ -1,1 +1,11 @@
-console.log("whats up bud");
+document.addEventListener("DOMContentLoaded",function(){
+    //This will store all the componenets from index.html into my JS
+    let operators = document.querySelectorAll('.operator');
+    let clear = document.querySelector('.clear');
+    let equal = document.querySelector('.equal');
+    let decimal = document.querySelector('.decimal');
+    let numbers = document.querySelectorAll('.number');
+
+    let previousScreen = document.querySelector('.previous');
+    let currentScreen = document.querySelector('.current');
+})
